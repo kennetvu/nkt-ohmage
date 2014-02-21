73f324356f98988fe16b5f2c0c2586252991f257
Kennet Vuong <Kennetvuong@gmail.com>
2014-02-21 14:04:38 +0100

    Test4

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
 app.use(express.session());
 app.use(app.router);
+
 if(app.get('env') == 'production'){
 	app.use('/nkt-ohmage',express.static(path.join(__dirname, 'public')));
 }
