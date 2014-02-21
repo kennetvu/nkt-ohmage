b457ee2de2ccaf408d7a0971ec0e004bc5d28305
Kennet Vuong <Kennetvuong@gmail.com>
2014-02-21 13:50:17 +0100

    Test for production server

--- app.js
+++ app.js
@@ -3,13 +3,13 @@
  * Module dependencies.
  */
 
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
+ var express = require('express');
+ var routes = require('./routes');
+ var user = require('./routes/user');
+ var http = require('http');
+ var path = require('path');
 
-var app = express();
+ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -23,16 +23,24 @@ app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
 app.use(express.session());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+if(app.get('env') == 'production'){
+	app.use('/nkt-ohmage',express.static(path.join(__dirname, 'public')));
+}
+else if (app.get('env') == 'development'){
+	app.use(express.static(path.join(__dirname, 'public')));
+}
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 }
+
+console.log(app.get('env'));
+console.log(app.settings.env);
 
 app.get('/', routes.index);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+	console.log('Express server listening on port ' + app.get('port'));
 });
